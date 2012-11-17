42088057cb6fbe61f06d10c5067dcdb8478a143b
Busy People <busypeoples@gmail.com>
2012-11-17 19:50:34 +0100

    Updated

--- view/footer.php
+++
@@ -1,5 +0,0 @@
-        <div class="footer">
-            <i>API DEMO &copy 2012</i>
-        </div>
-    </body>
-</html>
--- view/login.php
+++
@@ -1,5 +0,0 @@
-<?php echo $data['header'];?>
-<h1>Login Page</h1>
-<h4><?php echo $data['title'];?></h4>
-<p><?php echo $data['content'];?></p>
-<?php echo $data['footer'];?>
